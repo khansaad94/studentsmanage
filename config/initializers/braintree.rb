39526610bd5ddc75d7205accd340e9d5c3f3e533
tahirkhan109 <tahir_lodhi109@yahoo.com>
2016-11-10 14:25:59 +0500

    changed

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -13,9 +13,9 @@ module ActionView
   end
 end
 
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.logger = Logger.new('log/braintree.log')
-Braintree::Configuration.merchant_id = "fkd8bmztpwghmbfc"
-Braintree::Configuration.public_key = 't8yjc7dt7c5gjwdg'
-Braintree::Configuration.private_key = '387ac32f8109566c2c9dd04f4aec4bd4'
+# Braintree::Configuration.environment = :sandbox
+# Braintree::Configuration.logger = Logger.new('log/braintree.log')
+# Braintree::Configuration.merchant_id = "fkd8bmztpwghmbfc"
+# Braintree::Configuration.public_key = 't8yjc7dt7c5gjwdg'
+# Braintree::Configuration.private_key = '387ac32f8109566c2c9dd04f4aec4bd4'
 #Braintree::Configuration.client_side_encryption_key = 'MIIBCgKCAQEAu+QU/asiBsLnkaduG9fbSz8Y/KbwIsE6sinJg8z2XfEtecNMYRAvxAU/5qWWsK3InYgyG8fv96Z8W8uAfBDtNQHRfBkUVYukVYhb6aXjGiyw3HQkypX7XneLhZ6CVYg0ImPospR0i0E6VzSp/QqI+x+JycmqRN0T+8LlvvqL5Z7B+d7/dabTDIDZP6gP95oXEaRP'
